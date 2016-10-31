47eaae95c14f19c6925352a6f4447d3cd7941f65
HEDGEHOG\nicolas <nico1.delpech@gmail.com>
2016-10-31 12:56:57 +1000

    Changes to load balancer and packages.json

--- models/loadbalancer.js
+++ models/loadbalancer.js
@@ -1,11 +1,42 @@
 //This file will contain functions for contacting the loadbalancer
-var express = require('express')
+var express = require('express');
+var zmq = require('zmq');
+var os = require('os');
 
-function startStyleTransfer(contentimagepath, styleimagepath, callback) {
-	console.log(contentimagepath);
-	console.log(styleimagepath);
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
 
-	callback('everything is good from here');
+var loadBalancerIP = "127.0.0.1"
+
+function startStyleTransfer(contentImagePath, styleImagePath, callback) {
+	console.log(contentImagePath);
+	console.log(styleImagePath);
+	
+	var requester = zmq.socket('req');
+
+	requester.on("message", function(reply) {
+    		process.exit(0);
+	});
+
+	
+	requester.connect("tcp://"+loadBalancerIP+":5555");
+
+	//REmove next lane when not on local host. TO BE TESTED WITHOut!
+	addresses[0]="127.0.0.1";
+	
+	styleImagePath = addresses[0] + '/' + styleImagePath;
+	contentImagePath = addresses[0] + '/' + contentImagePath;
+  	requester.send(addresses[0] + "#" + styleImagePath + "#" + contentImagePath);
+
+	callback('Style transfer started');
 	//When the function is done, the function callback can be used to deliver the result
 }
 
