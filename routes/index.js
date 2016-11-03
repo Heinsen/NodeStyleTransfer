083442f3f8d2dbfbc6e39c1961e458959e504ad8
Jørgen Christian Heinsen <c.heinsen@gmail.com>
2016-11-03 22:13:16 +1000

    Small changes to the pathing system. Added url params to parse content and style image file names to /uploadimage.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
 
-var express      = require('express');
-var cookieParser = require('cookie-parser');
-
-app.use(cookieParser());
+var loadbalancer = require('../models/loadbalancer.js');
 
 var fs = require("fs");
 var multer  = require('multer');
@@ -21,27 +17,28 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage });
 
-var loadbalancer = require('../models/loadbalancer.js');
-
-var contentImagePath;
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	//console.log('Cookies: ', req.cookies);
-  	res.render('index', {uploadedImagePath: '/images/resources/white_square.png'});
-})
+	res.render('index', {uploadedImagePath: '/images/resources/white_square.png'});
+});
 
 /* Upload content image */
 router.post('/uploadimage', upload.single('file'), function(req, res, next) {
-	res.render('index', { uploadedImagePath: req.file.path } );
+	if(req.file != undefined) {
+		res.render('index', { uploadedImagePath: req.file.path } );
+	}
+	else {
+		res.render('index');
+	}
 });
 
 router.get('/startstyletransfer', function(req, res, next) {
 
-	var publicimagePath = __dirname + '../public/images/';
+	var contentImage = req.query.content_image;
+	var styleImage = req.query.style_image;
 
-	loadbalancer.startstyletransfer(publicimagePath + 'content/' + contentImagePath,
-									publicimagePath + 'style/art_the_great_wave_off_kanagawa.jpg',
+	loadbalancer.startstyletransfer(contentImage,
+									styleImage,
 									function(result) {
   		res.render('styletransferprogress', {});
 	});
